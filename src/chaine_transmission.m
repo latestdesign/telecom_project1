7473c5971358e46b41991b5721b605b17b8fa660
Nkoko77 <bailliet.nicolas@gmail.com>
2024-05-30 15:24:42 +0200

    pammod

--- src/chaine_transmission.m
+++ src/chaine_transmission.m
@@ -1,4 +1,4 @@
-function [TEB, s, s_transp, symboles, s_sample, nb_symb, Ns] = chaine_transmission(n, SNRB, N, Fe, fp, Rb, pbEquivalent)
+function [TEB, s, s_transp, symboles, s_sample, nb_symb, Ns] = chaine_transmission(n, SNRB, N, Fe, fp, Rb, pbEquivalent, ask)
     % - n: nombre de bits par symbole
     % - SNRB: rapport signal sur bruit par bit
     % - N: nombre de bits total transmis
@@ -23,8 +23,10 @@ function [TEB, s, s_transp, symboles, s_sample, nb_symb, Ns] = chaine_transmissi
     bits_regroupes = reshape(bits, n, []);
     entiers = bi2de(bits_regroupes.', 'left-msb');
     if ask 
-        symboles = pammod
-    symboles = pskmod(entiers, M).'; % éventuellement mettre en Gray
+        symboles = pammod(entiers, M).';
+    else 
+        symboles = pskmod(entiers, M).'; % éventuellement mettre en Gray
+    end
     x = kron(symboles, [1 zeros(1, Ns-1)]);
 
     % FILTRE EMISSION
